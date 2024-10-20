f51094edd7c009e48428921a06f36ca2d130f5e8
Rajeev Sharma <rajeev.s.2403@gmail.com>
2024-10-20 20:37:47 +0530

    refactored training form

--- frontend/src/Components/ModelTrainForm.jsx
+++ frontend/src/Components/ModelTrainForm.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from "react"
 import JSZip from "jszip"
 import axios from "axios"
-
+import { Upload } from "lucide-react"
 import { Button } from "./UI/Button"
 
 function ModelTrainForm() {
     const [modelName, setModelName] = useState("")
+    const [isDragging, setIsDragging] = useState(false)
     const [triggerWord, setTriggerWord] = useState("MOD")
     const [selectedFiles, setSelectedFiles] = useState([])
 
@@ -15,7 +16,20 @@ function ModelTrainForm() {
         const files = Array.from(e.target.files)
         setSelectedFiles((prevFiles) => [...prevFiles, ...files])
     }
-
+    function handleDragOver(e) {
+        e.preventDefault()
+        setIsDragging(true)
+    }
+    function handleDragLeave(e) {
+        e.preventDefault()
+        setIsDragging(false)
+    }
+    function handleDrop(e) {
+        e.preventDefault()
+        setIsDragging(false)
+        const droppedImage = e.dataTransfer.files[0]
+        setSelectedFiles((prevFiles) => [...prevFiles, droppedImage])
+    }
     async function handleSubmit(e) {
         e.preventDefault()
         if (selectedFiles.length === 0) return
@@ -57,85 +71,121 @@ function ModelTrainForm() {
     }
 
     return (
-        <div className="max-w-xl mx-auto p-4 bg-white shadow-md rounded-lg">
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                        Model Name
-                    </label>
-                    <input
-                        type="text"
-                        value={modelName}
-                        onChange={(e) => setModelName(e.target.value)}
-                        required
-                        className="px-3 py-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
-                    />
-                </div>
-                <div>
-                    <label
-                        htmlFor="images"
-                        className="block text-sm font-medium text-gray-700 mb-2"
-                    >
-                        Select Images
-                    </label>
-                    <input
-                        type="file"
-                        id="images"
-                        ref={fileInputRef}
-                        onChange={handleFileChange}
-                        multiple
-                        accept="image/*"
-                        className="block w-full text-sm text-gray-500
-              file:mr-4 file:py-2 file:px-4
-              file:rounded-full file:border-0
-              file:text-sm file:font-semibold
-              file:bg-slate-50 file:text-slate-700
-              hover:file:bg-slate-100"
-                    />
-                </div>
-                <div>
-                    <p className="text-sm text-gray-600 mb-2">
-                        Selected images: {selectedFiles.length}
+        <div className="max-w-sm h-[90dvh] mx-auto  bg-white border-r-[1px] relative">
+            <div className="h-[90%] max-w-sm m-0 px-4 py-4 overflow-y-scroll oveflow-x-hidden">
+                <div className="font-sans flex flex-col gap-1 h-max pb-6 border-b-[1px] ">
+                    <h1 className="font-sans font-semibold tracking-tighter text-xl text-gray-800">
+                        Train Model
+                    </h1>
+                    <p className="text-xs font-regular tracking-tight text-gray-500">
+                        Train your images to help the AI get to know you, so it
+                        can create images just the way you like!
                     </p>
-                    <div className="grid grid-cols-3 gap-2">
-                        {selectedFiles.slice(0, 3).map((file, index) => (
-                            <div key={index} className="relative">
-                                <img
-                                    src={URL.createObjectURL(file)}
-                                    alt={`Preview ${index + 1}`}
-                                    className="w-full h-24 object-cover rounded"
-                                />
-                                <button
-                                    type="button"
-                                    onClick={() => removeFile(index)}
-                                    className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs"
-                                >
-                                    &times;
-                                </button>
-                            </div>
-                        ))}
+                </div>
+                <form onSubmit={handleSubmit} className="mt-4">
+                    <div className="flex flex-col h-max pb-2">
+                        <label className="block text-sm font-medium text-gray-700">
+                            Model Name
+                        </label>
+                        <p className="text-[0.675rem] font-regular tracking-tight text-gray-500">
+                            Pick a name for your model
+                        </p>
+
+                        <input
+                            type="text"
+                            value={modelName}
+                            onChange={(e) => setModelName(e.target.value)}
+                            required
+                            className="px-3 py-2 mt-2 block w-full border border-gray-700 rounded-md  focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
+                        />
                     </div>
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                        Trigger Word
-                    </label>
-                    <input
-                        type="text"
-                        value={triggerWord}
-                        onChange={(e) => setTriggerWord(e.target.value)}
-                        className="px-3 py-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
-                    />
-                </div>
+                    <div className="flex flex-col h-max pb-2 mt-1">
+                        <label className="block text-sm font-medium text-gray-700">
+                            Trigger Word
+                        </label>
+                        <p className="text-[0.675rem] font-regular tracking-tight text-gray-500">
+                            Choose a trigger word for the model to generate
+                            images using the provided images. It should be a
+                            unique term that doesn't have any meaning in natural
+                            language.
+                        </p>
+                        <input
+                            type="text"
+                            value={triggerWord}
+                            onChange={(e) => setTriggerWord(e.target.value)}
+                            className="px-3 py-2 mt-2 block w-full border border-gray-700 rounded-md  focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
+                        />
+                    </div>
+                    <div className="flex flex-col h-max pb-2 mt-1">
+                        <p className="block text-sm font-medium text-gray-700">
+                            Upload Images
+                        </p>
+                        <p className="text-[0.675rem] font-regular tracking-tight text-gray-500">
+                            For the best results, upload at least 5 images of
+                            yourself from different angles and poses.
+                        </p>
+                    </div>
+                    <div
+                        className={`w-full h-32 mx-auto border-2 border-dashed rounded-lg flex flex-col items-center justify-center cursor-pointer ${isDragging ? "border-blue-500 bg-blue-50" : "border-gray-300"}`}
+                        onDragOver={handleDragOver}
+                        onDragLeave={handleDragLeave}
+                        onDrop={handleDrop}
+                    >
+                        <Upload className="w-8 h-8 text-gray-400 mb-2" />
+                        <p className="text-sm text-gray-500">
+                            Drag your file(s) or{" "}
+                            <label
+                                htmlFor="images"
+                                className="text-blue-500 cursor-pointer"
+                            >
+                                browse
+                                <input
+                                    type="file"
+                                    id="images"
+                                    ref={fileInputRef}
+                                    onChange={handleFileChange}
+                                    multiple
+                                    accept="image/*"
+                                    className="hidden"
+                                />
+                            </label>
+                        </p>
+                    </div>
+                    <div>
+                        <p className="text-sm text-gray-600 mb-2 py-2">
+                            Selected images: {selectedFiles.length}
+                        </p>
+                        <div className="max-h-28 grid grid-cols-3 gap-2 ">
+                            {selectedFiles.map((file, index) => (
+                                <div key={index} className="relative">
+                                    <img
+                                        src={URL.createObjectURL(file)}
+                                        alt={`Preview ${index + 1}`}
+                                        className="w-full h-24 object-cover rounded"
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={() => removeFile(index)}
+                                        className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs"
+                                    >
+                                        &times;
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div className="w-full h-[10%] flex justify-center items-center border-t-[1px]">
                 <Button
                     type="button"
                     onClick={handleSubmit}
                     disabled={selectedFiles.length === 0 && modelName === ""}
-                    className="w-full"
+                    className="w-2/3 mx-auto"
                 >
                     Train Model
                 </Button>
-            </form>
+            </div>
         </div>
     )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,1 +1,12 @@
-export default function Home() {}
+import ModelTrainForm from "../Components/ModelTrainForm"
+function Home(){
+
+    return (
+        <div className="h-max w-max">
+             <div className="abosolute h-[90dvh] w-max top-[10dvh] left-0">
+                 <ModelTrainForm />
+            </div>
+        </div>
+    )
+}
+export default Home;
