610efbf0f2a9d0e2251a6f1a4bd92952fb831d78
Rajeev Sharma <rajeev.s.2403@gmail.com>
2024-10-21 22:25:15 +0530

    add login page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {
-            fontFamily:{
-                sans: ['Inter', 'sans-serif'],
-                heading:['Arima', 'sans-serif']
-            }
-        },
+    	extend: {
+    		fontFamily: {
+    			sans: ['Inter', 'sans-serif'],
+    			heading: ['Arima', 'sans-serif']
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {}
+    	}
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 }
