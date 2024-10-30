358db123d4caf3a6fb8b4d685e603f2ce89805a0
Sarthak <sarthakrawatbiz@gmail.com>
2024-10-30 23:39:57 +0530

    Added route for getting trigger word

--- server/routes/replicateRoutes.js
+++ server/routes/replicateRoutes.js
@@ -4,6 +4,7 @@ import {
   trainOstrisModel,
   runUserModel,
   getTrainingStatus,
+  getTriggerWord,
 } from "../controllers/replicateController.js";
 import upload from "../middlewares/uploadMiddleware.js";
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post("/train", upload.single("file"), trainOstrisModel);
 router.post("/status", getTrainingStatus);
 router.post("/run", runUserModel);
+router.get("/get-trigger-word", getTriggerWord);
 
 export default router;
